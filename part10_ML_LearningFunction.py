b6420a412e6459ce6be66b9643173a2c5fbf1888
SebasVM <sebastiaan.westdijk@eneco.com>
2017-04-14 11:15:24 +0200

    On hold.

--- part10_ML_LearningFunction.py
+++ part10_ML_LearningFunction.py
@@ -16,3 +16,15 @@ def process_data_for_labels(ticker):
 	return tickers, df
 
 process_data_for_labels('XOM')
+
+def buy_sell_hold(*args):
+	cols = [c for c in args]
+	requirement = 0.02 
+	for col in cols:
+		if col > requirement:
+			return 1
+		if col < -requirement:
+			return -1
+
+	return 0
+
